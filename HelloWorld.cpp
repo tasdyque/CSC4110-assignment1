2794e2f5592f7771bd2bc472788e443229954d62
Tasdyque <tasdyque.syed@gmail.com>
2022-09-20 22:28:06 -0400

    Changing the greetings

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -8,8 +8,8 @@ using namespace std;
 // where the execution of program begins 
 int main() 
 { 
-    // prints hello world 
+    // Testing git merge!
     cout<<"Hello World!"; 
-      
+    
     return 0; 
 } 
